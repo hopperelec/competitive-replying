5ac60b9a83dcad421507dddeeed75291b89439e5
hopperelec <hopperelec@gmail.com>
2024-08-29 19:51:00 +0100

    Only show login button if the user is not signed in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
-<script>
+<script lang="ts">
 import LoginButton from "$lib/components/LoginButton.svelte";
+import type {PageData} from "./$types";
+
+export let data: PageData;
 </script>
 
-<LoginButton/>
+{#if data.session == null}
+    <LoginButton/>
+{:else}
+  <p>Welcome, {data.session.user?.name ?? "User"}!</p>
+{/if}
